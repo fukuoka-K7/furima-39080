bfe8cdb8957c72debd6ffd020ba2c47fb6c2857d
fukuoka-K7 <sekaidayoon@gmail.com>
2023-04-18 21:27:12 +0900

    商品出品機能修正②

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,11 +12,6 @@ class Item < ApplicationRecord
   validates :image,             presence: true
   validates :item_name,         presence: true
   validates :description,       presence: true
-  validates :category_id,       
-  validates :condition_id,      
-  validates :shipping_burden_id,
-  validates :shipping_from_id  ,
-  validates :shipping_day_id,   
   validates :price,             presence: true, numericality: {only_integer: true}, inclusion: { in: 300..9999999 }
 
     with_options numericality: { other_than: 1 } do
