c648058625663ad39f804e891cc9f86e17c93805
fukuoka-K7 <sekaidayoon@gmail.com>
2023-04-27 23:05:58 +0900

    日本語エラー文化

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -14,67 +14,67 @@ RSpec.describe Item, type: :model do
       it '写真が空では出品できない' do
         @item.image = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("Image can't be blank")
+        expect(@item.errors.full_messages).to include("商品画像を入力してください")
       end
       it '商品名が空では出品できない' do
         @item.item_name = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Item name can't be blank")
+        expect(@item.errors.full_messages).to include("商品名を入力してください")
       end
       it '説明が空では出品できない' do
         @item.description = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Description can't be blank")
+        expect(@item.errors.full_messages).to include("商品の説明を入力してください")
       end
       it 'カテゴリーは「---」が選択されているときは出品できない' do
         @item.category_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include('Category must be other than 1')
+        expect(@item.errors.full_messages).to include('カテゴリーを選択してください')
       end
       it '商品の状態は「---」が選択されているときは出品できない' do
         @item.condition_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include('Condition must be other than 1')
+        expect(@item.errors.full_messages).to include('商品の状態を選択してください')
       end
       it '送料がは「---」が選択されているときは出品できない' do
         @item.shipping_burden_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include('Shipping burden must be other than 1')
+        expect(@item.errors.full_messages).to include('配送料の負担を選択してください')
       end
       it '発送元は「---」が選択されているときは出品できない' do
         @item.shipping_from_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include('Shipping from must be other than 1')
+        expect(@item.errors.full_messages).to include('発送元の地域を選択してください')
       end
       it '発送までの日数は「---」が選択されているときは出品できない' do
         @item.shipping_day_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include('Shipping day must be other than 1')
+        expect(@item.errors.full_messages).to include('発送までの日数を選択してください')
       end
       it '価格が空では出品できない' do
         @item.price = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price can't be blank")
+        expect(@item.errors.full_messages).to include('販売価格を入力してください')
       end
       it '価格が300円未満では出品できない' do
         @item.price = '299'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price is not included in the list')
+        expect(@item.errors.full_messages).to include('販売価格は一覧にありません')
       end
       it '価格が10,000,000では出品できない' do
         @item.price = '100000000'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price is not included in the list')
+        expect(@item.errors.full_messages).to include('販売価格は一覧にありません')
       end
       it '全角では価格が登録できない' do
         @item.price = '１０００'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price is not included in the list')
+        expect(@item.errors.full_messages).to include('販売価格は数値で入力してください')
       end
       it 'ユーザーが紐づいていないと登録できない' do
         @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include('User must exist')
+        expect(@item.errors.full_messages).to include('Userを入力してください')
       end
     end
   end
